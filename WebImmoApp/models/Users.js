f117812b36be5c0d07daf281c17e0d0f9164c850
kevisam <kevin.sam@vub.be>
2021-10-17 12:49:44 +0200

    Connected to db

--- WebImmoApp/models/Users.js
+++ WebImmoApp/models/Users.js
@@ -5,11 +5,44 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    name:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: Number,
+        required: true
+    },
 
     email:{
         type: String,
         required: true
     },
+    address:{
+        type: String,
+        required: true
+    },
+
+    postcode:{
+        type: Number,
+        required: true
+    },
+    municipality:{
+        type: String,
+        required: true
+    },
+    agency:{
+        type: String,
+    },
+    IPINumber:{
+        type: Number,
+    },
+
+    
 })
 
 const User = mongoose.model('User', UserSchema);
--- WebImmoApp/routes/users.js
+++ WebImmoApp/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 
 // User model
 
@@ -15,10 +15,39 @@ router.get('/register', (req,res) => res.render('register'));
 // Register Handle
 
 router.post('/register', (req,res) => {
-    //const {title, surname, name, phone, email, address, postcode, municipality, agency, ipiNumber } = req.body;
+    const {title, surname, name, phone, email, address, postcode, municipality, agency, ipiNumber } = req.body;
 
-    console.log(req.body)
-    res.send('Hello world')
+    /*TODO
+     Check if every field is filled in
+      if {} else {
+          ... Here put code that comes underneath
+      }
+    */
+
+      User.findOne({email:email}).then(user => {
+          if(user) {
+              // User exists
+
+          } else {
+              // Here we create the user in the database
+              const newUser = new User({
+                title,
+                surname, 
+                name, 
+                phone, 
+                email, 
+                address, 
+                postcode, 
+                municipality, 
+                agency, 
+                ipiNumber 
+              });
+
+              console.log(newUser);
+              res.send('hello');
+
+          }
+      });
 });
 
 module.exports = router;
